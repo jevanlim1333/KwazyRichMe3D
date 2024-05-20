af8c9eaceda7244de0411e45bb78efafc06bc6ab
jevan <jlimse.1333@gmail.com>
2024-05-20 15:52:30 +0800

    Test

---
+++ Assets/Scripts/TokenScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+    public string tokenName;
+    public Tile position;
+
+}
