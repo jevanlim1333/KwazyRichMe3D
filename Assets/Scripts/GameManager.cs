c0b72d72dddfb1ac67cd90a21fb1c12b68a382e3
jevan <jlimse.1333@gmail.com>
2024-05-21 23:07:07 +0800

    Started adding Token

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,19 @@ namespace Com.MyCompany.MyGame
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        void Start() 
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
+            }
+        }
 
         #region Photon Callbacks
 
@@ -48,6 +61,14 @@ namespace Com.MyCompany.MyGame
 
         #endregion
 
+        #region Public Fields
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+
+
+        #endregion
+
         #region Public Methods
 
         public void LeaveRoom()
@@ -71,5 +92,7 @@ namespace Com.MyCompany.MyGame
         }
 
         #endregion
+
+        
         }
 }
