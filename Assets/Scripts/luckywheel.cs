e7540b7bf73a76f462f65f41421c244252555031
jevan <jlimse.1333@gmail.com>
2024-07-10 22:28:24 +0800

    Fixed LuckyWheel

--- Assets/Scripts/luckywheel.cs
+++ Assets/Scripts/luckywheel.cs
@@ -30,9 +30,7 @@ public class luckywheel : MonoBehaviour
             });
             pickerWheel.Spin();
         });
-
-        wheelCanvas.GetComponent<Canvas>().enabled = false;
-        backgroundCanvas.GetComponent<Canvas>().enabled = false;    
+ 
     }
 
     public void openTreats()
@@ -41,13 +39,41 @@ public class luckywheel : MonoBehaviour
         
         wheelCanvas.GetComponent<Canvas>().enabled = true;
         backgroundCanvas.GetComponent<Canvas>().enabled = true;
+
+        CanvasGroup wheelCanvasGroup = wheelCanvas.GetComponent<CanvasGroup>();
+        CanvasGroup backgroundCanvasGroup = backgroundCanvas.GetComponent<CanvasGroup>();
+
+        wheelCanvasGroup.alpha = 1;
+        wheelCanvasGroup.interactable = true;
+        wheelCanvasGroup.blocksRaycasts = true;
+
+        backgroundCanvasGroup.alpha = 1;
+        backgroundCanvasGroup.interactable = true;
+        backgroundCanvasGroup.blocksRaycasts = true;
+
+
+        Debug.Log("Opening treats canvas now");
     }
     public void closeTreats()
     {
         //UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
-
+        
         wheelCanvas.GetComponent<Canvas>().enabled = false;
-        backgroundCanvas.GetComponent<Canvas>().enabled = false;    
+        backgroundCanvas.GetComponent<Canvas>().enabled = false;
+
+        CanvasGroup wheelCanvasGroup = wheelCanvas.GetComponent<CanvasGroup>();
+        CanvasGroup backgroundCanvasGroup = backgroundCanvas.GetComponent<CanvasGroup>();
+
+        wheelCanvasGroup.alpha = 0;
+        wheelCanvasGroup.interactable = false;
+        wheelCanvasGroup.blocksRaycasts = false;
+
+        backgroundCanvasGroup.alpha = 0;
+        backgroundCanvasGroup.interactable = false;
+        backgroundCanvasGroup.blocksRaycasts = false;
+
+
+        Debug.Log("Closing treats canvas now");
     }
 
 }
